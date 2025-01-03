9dc4f26a2d23592fe19e8f99915f879a3e8e0852
champark <97137143+champark@users.noreply.github.com>
2025-01-03 12:13:55 +0900

    완성

---
+++ CRUD4_MEMO/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO memo(content, created_At) VALUES ('테스트메모1', NOW());
+INSERT INTO memo(content, created_At) VALUES ('테스트메모2', NOW());
+INSERT INTO memo(content, created_At) VALUES ('테스트메모3', NOW());
+INSERT INTO memo(content, created_At) VALUES ('테스트메모4', NOW());
+INSERT INTO memo(content, created_At) VALUES ('테스트메모5', NOW());
+INSERT INTO memo(id, content, created_At) VALUES (6, '테스트메모5', NOW());
